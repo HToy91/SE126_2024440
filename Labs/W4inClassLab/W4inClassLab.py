fbb0414e87506fa3da59793043e72632edfbd6dc
HToy91 <jfvargasfigueroa@email.neit.edu>
2024-08-07 10:20:52 -0400

    W4 In Class Lab

--- Labs/W4inClassLab/W4inClassLab.py
+++ Labs/W4inClassLab/W4inClassLab.py
@@ -2,9 +2,26 @@
 #SE126 Lab 4a
 #08/07/2024
 
-#PROGRAM PROMPT: 
+#PROGRAM PROMPT:
+#Part 1: Write a program that reads the data file (below) and stores the data into 1D parallel lists (remember, one list for every field).  Once the lists are populated (and the file is "closed"), process the lists to reprint the file data, record by record as they originally appear in the file.
+
+#Part 2: Next, reprocess the lists to find each student's current average score along with the class average.  While processing in the for loop, store each student's average into a new list called 'avg' and reprint the records to include this numeric average.  Reprocess the lists one final time to find the letter equivalent of each student's average and store this into a new list called 'let_avg'.  Reprint the lists for a third time, record by record including average score and average letter equivalent.  After this third print of the original file data, print to the console the total number of student's in the class along with the class numeric average.  
+
+#Part 3: After your final display using the 1D parallel lists, create one new, empty list, that will become a 2D list.  Then, using a for loop and the 1D parallel lists, store the data to the 2D list you have created. Each index in the 2D list should contain a list of data. This list of data should contain the fname, lname, test1, test2, test3, num_average, and letter_average for the respective student. Process through this 2D list to display the data from the file (it should appear just like your previous 3 prints!)
 
 import csv
+#VARIABLE and LIST DICTIONARY
+#firstNames = []
+#lastNames = []
+#test1 = []
+#test2 = []
+#test3 = []
+#numAvg = []
+#letAvg = []
+#avg = (test1[i] + test2[i] + test3[i]) / 3
+#gradeTotal = 0
+#classAvg = gradeTotal / len(numAvg)
+
 
 #create empty lists - one for EACH potential field
 firstNames = []
